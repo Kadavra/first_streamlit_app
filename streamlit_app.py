36fee03fe54f1c270823fc77314187f93f70a3be
Kadavra <kevin.faucherand@gmail.com>
2023-08-01 15:02:35 +0200

    Update streamlit_app.py
    Add New Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled free range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
