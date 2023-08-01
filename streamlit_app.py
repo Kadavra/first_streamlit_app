f5f2bff476b99d7dd30376176f9a2625ebf001f7
Kadavra <kevin.faucherand@gmail.com>
2023-08-01 15:01:36 +0200

    Update streamlit_app.py
    Add Emoji + Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('My parent new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled free range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled free range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
